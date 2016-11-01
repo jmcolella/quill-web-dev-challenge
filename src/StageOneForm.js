c47d68009c27bcca0437f670dbe110108d3414a0
John Colella <colella.john@gmail.com>
2016-11-01 12:40:37 -0400

    add StageOneForm component

---
+++ src/StageOneForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { initialPassage } from './actions/actions';
+
+const StageOneForm = ( props, context ) => {
+  let input;
+
+  return (
+    <div>
+      <textarea
+          ref={ node => {
+            input = node
+          }}
+          placeholder="Add gramtically correct passage">
+      </textarea>
+
+      <button
+        onClick={
+          () => props.dispatch(initialPassage( input.value ) )
+        }
+      >
+        Next
+      </button>
+
+    </div>
+  )
+};
+
+export default connect()(StageOneForm);
