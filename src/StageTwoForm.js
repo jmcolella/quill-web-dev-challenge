bce4613e42480f48d8bedf204917427a0f584e75
John Colella <colella.john@gmail.com>
2016-11-02 12:01:51 -0400

    add StageTwoForm component with necessary onChange handler for updating passage

---
+++ src/StageTwoForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { changeInitialPassage } from './actions/app_actions';
+
+const StageTwoForm = ( props, context ) => {
+  let input;
+
+  return (
+    <div>
+    <h1> Stage Two</h1>
+      <textarea
+          ref={ node => {
+            input = node
+          }}
+          onChange={
+            () => props.dispatch( changeInitialPassage( input.value ) )
+          }
+          value={
+            props.change.passage === "" ? props.initial.passage : props.change.passage
+          }
+      >
+      </textarea>
+
+      <button
+        // onClick={
+        //   () => props.dispatch(comparePassages( state.initial.passage, input.value ) )
+        // }
+      >
+        Next
+      </button>
+
+    </div>
+  )
+};
+
+
+export default connect()(StageTwoForm);
