1a6ccef786d280a733cb33cb8d097bd130a791bc
John Colella <colella.john@gmail.com>
2016-11-03 08:50:28 -0400

    add shell component for Stage Three

---
+++ src/StageThree.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const StageThree = ( props, context ) => {
+  debugger;
+  return (
+    <div>
+      Stage Three
+    </div>
+  )
+};
+
+export default connect()(StageThree);
