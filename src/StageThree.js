dbaa40878519c8a10397652cd4a61366c1a0d82b
John Colella <colella.john@gmail.com>
2016-11-06 19:22:06 -0500

    update Stage Three form with form view

--- src/StageThree.js
+++ src/StageThree.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import CompareItem from './CompareItem';
+import './StageThree.css';
+import'./App.css';
 
-const StageThree = ( props, context ) => {
-  debugger;
-  return (
-    <div>
-      Stage Three
-    </div>
-  )
+class StageThree extends Component {
+  render() {
+    let { compare } = this.props;
+    return (
+      <div className="container">
+        <form>
+          <div className="heading">
+            <div className="form-items">
+              <label className="form-heading">Initial</label>
+            </div>
+            <div className="form-items">
+              <label className="form-heading">Change</label>
+            </div>
+            <div className="form-items">
+              <label className="form-heading">Concept</label>
+            </div>
+          </div>
+          {
+            compare.comparison.map( ( item, index ) =>  {
+              return <CompareItem
+                        key={ index }
+                        id={ index }
+                        data={ item } />
+            })
+          }
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    );
+  };
 };
 
 export default connect()(StageThree);
