8d40668483487dcd387d0c82c33ced69120f67ae
John Colella <colella.john@gmail.com>
2016-11-06 22:29:53 -0500

    update App.test with necessary redux components to make example test pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from 'redux';
+import { passageApp } from './reducers/app_reducers';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={ createStore( passageApp ) }>
+      <App />
+    </Provider>,
+    div
+  );
 });
